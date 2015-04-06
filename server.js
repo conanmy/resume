646b5e44b28872919d27146bc9c0d1a1e8be47ad
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-04-07 00:02:19 +1200

    update mongo

--- server.js
+++ server.js
@@ -37,8 +37,10 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 
     app.post('/resume/all', function(req, res) {
         if (req.body._id) {
+            var _id = req.body._id;
+            delete req.body._id;
             resumes.update({
-                '_id': new mongodb.BSONPure.ObjectID(req.body._id)
+                '_id': new mongodb.BSONPure.ObjectID(_id)
             }, req.body, function(err, result) {
                 if (err) {
                     res.send(err);
