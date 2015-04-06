f97ef81e4c4a4f31938964ea2cd8c158c4572b48
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-04-06 22:45:15 +1200

    change get resume param

--- client/app/resume/service.js
+++ client/app/resume/service.js
@@ -1,7 +1,7 @@
 angular.module('resume')
     .factory('getResumeService', ['$resource', function($resource) {
         return $resource(
-            '/resume/all:resumeId',
+            '/resume/all/:resumeId',
             {resumeId: '@id'}
         );
     }]);
