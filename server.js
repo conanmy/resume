f1586dec6732ebdb6465e49e11f7bd0cbca97807
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-04-23 14:31:34 +1200

    remove put

--- server.js
+++ server.js
@@ -44,16 +44,7 @@ mongodb.MongoClient.connect(uri, function(err, db) {
     });
 
     app.put('resumes/:resumeId', function() {
-        var _id = req.body._id;
-        delete req.body._id;
-        resumes.update({
-            '_id': new mongodb.BSONPure.ObjectID(_id)
-        }, req.body, function(err, result) {
-            console.log(err);
-            if (err) {
-                res.send(err);
-            }
-        });
+
     });
 
     app.delete('/resumes/:resumeId', function(req, res) {
